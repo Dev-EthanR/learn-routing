c900125bf9099361399ab9621bb1197bebdb9aa3
Ethan <ethanlegend7@gmail.com>
2026-01-05 15:51:23 +1100

    startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import About from "./Components/About";
-import Home from "./Components/Home";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VANLIFE
+        </Link>
+
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
